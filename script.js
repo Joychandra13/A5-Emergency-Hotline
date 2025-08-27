67d6907d3750ffb03aeeb93d54ccbb0e44f61adb
Joychandra13 <joychandra1313@gmail.com>
2025-08-27 16:13:41 +0600

    coin count and alert add

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ for (let i = 0; i < hearts.length; i++) {
     });
 }
 
+
+//coin count and alert
+const coinCounter = document.getElementById("coin-number");
+const callButtons = document.querySelectorAll("#call-btn");
+
+for (let i = 0; i < callButtons.length; i++) {
+    callButtons[i].addEventListener("click", function(e) {
+        e.preventDefault();
+
+        let coins = parseInt(coinCounter.textContent);
+        const card = callButtons[i].closest(".card"); 
+        const serviceName = card.querySelector(".service-name").innerText;
+        const serviceNumber = card.querySelector(".service-number").innerText;
+
+        if (coins < 20) {
+            alert("❌ You don't have enough coins to make this call.");
+            return;
+        }
+
+        coins -= 20;
+        coinCounter.textContent = coins;
+
+        alert(`📞 Calling ${serviceName}: ${serviceNumber}...`);
+
+    });
+}
+
